88e3f2da86f5045eb28e0db361c243e5936b3f38
dileepd53 <68851145+dileepd53@users.noreply.github.com>
2020-07-28 15:24:56 +0530

    simplified code

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,37 @@
 
-bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  //int i;
-  if(bpm < 70 || bpm > 150) {
-    return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
+
+bool isbpmOk (float bpm)
+{
+  if(bpm < 70 || bpm > 150){
     return false;
   }
   return true;
+}
+
+bool isSpo2Ok (float spo2)
+{
+  if(spo2 < 80) {
+    return false;
+  }
+  return true;
+}
+
+bool isRespRate (float respRate)
+{
+  if(respRate < 30 || respRate > 60){
+    return false;
+  }
+  return true;
+  
+}
+
+bool vitalsAreOk(float bpm, float spo2, float respRate) {
+  
+  bool bpmValue = isbpmOk(bpm);
+  
+  bool spo2Value = isSpo2Ok(spo2);
+  
+  bool respRateValue = isRespRate(respRate);
+  
+  return (bpmValue && spo2Value && respRateValue);
 }
